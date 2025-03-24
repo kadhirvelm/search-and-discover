7d1e35a9118c8af87c6ab5bb8a85d7a0a220cd89
Kadhirvel Manickam <kadhirvm@amazon.com>
2025-03-24 14:14:55 -0700

    Add nextjs

---
+++ typescript/packages/nextjs/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
+
+import "@ant-design/v5-patch-for-react-19";
+
+const openSans = Open_Sans({
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Search and discovery",
+	description: "Look for cool things",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={openSans.className}>{children}</body>
+		</html>
+	);
+}
