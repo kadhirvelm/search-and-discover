ce3afd7b868d9014baeb11e2015528d46c0cec2a
Kadhirvel Manickam <kadhirvm@amazon.com>
2025-03-24 15:38:26 -0700

    Add config handler

--- typescript/packages/nestjs/src/app.controller.ts
+++ typescript/packages/nestjs/src/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 // biome-ignore lint/style/useImportType: <explanation>
 import { AppService } from "./app.service";
+import type { SearchAndDiscoverConfigWithName } from "./config.service";
 
-@Controller()
+@Controller("app")
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@Get("get-configs")
+	getConfigs() {
+		return this.appService.getAllConfigFiles();
+	}
+
+	@Post("update-config")
+	updateConfig(config: SearchAndDiscoverConfigWithName) {
+		return this.appService.updateConfig(config.name, config.file);
 	}
 }
