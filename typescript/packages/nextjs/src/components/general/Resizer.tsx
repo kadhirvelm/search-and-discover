b122889234ee991d18a33356a95e4ebaf876d798
Kadhirvel Manickam <kadhirvm@amazon.com>
2025-03-31 10:37:55 -0700

    Flush out the UI

--- typescript/packages/nextjs/src/components/general/Resizer.tsx
+++ typescript/packages/nextjs/src/components/general/Resizer.tsx
@@ -1,9 +1,24 @@
 import { Flex } from "antd";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const Resizer = ({ children }: { children: (props: { width: number; height: number }) => React.ReactElement }) => {
     const container = useRef<HTMLDivElement>(null);
     const [hasMounted, setHasMounted] = useState(false);
+
+    // This is a hack to force a re-render when the window is resized
+    const [_, setRenderKey] = useState(0);
+
+    const updateRenderKey = useCallback(() => {
+        setRenderKey((key) => key + 1);
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("resize", updateRenderKey)
+
+        return () => {
+            window.removeEventListener("resize", updateRenderKey);
+        }
+    }, [updateRenderKey]);
 
     useEffect(() => {
         setHasMounted(true);
